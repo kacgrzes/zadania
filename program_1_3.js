f678b1d07b443de7a99fad07ad1e4ae85a9a079b
szopizzle <maciejdabek@gmail.com>
2017-02-03 10:53:39 +0100

    wciecia poprawione
     pomysle nad tym jak ustrzec sie przed lang-undefine

--- program_1_3.js
+++ program_1_3.js
@@ -1,23 +1,28 @@
-process.stdin.setEncoding("utf-8");
+process.stdin.setEncoding('utf-8');
 
 console.log('type /exit to exit,/ver to display node version , /lang for system language\n')  
 
-process.stdin.on("readable", function(){
+process.stdin.on('readable', function(){
 var input= process.stdin.read();
 
 if(input !== null){
   var instruction=input.toString().trim();
 
   switch(instruction){ 
-  case "/exit":  process.stdout.write("bye bye\n");
-    process.exit();
-   break;
-   
-  case "/ver":  process.stdout.write("your node version is " + process.versions.node+"\n");
-  break;
- 
-  case "/lang":process.stdout.write("your system language is " + process.env.LANG+"\n"); 
-  break;
+    case '/exit':  
+      process.stdout.write('bye bye\n');
+      process.exit();
+      break;
 
-  default:process.stderr.write("wrong instruction\n");}
+    case '/ver':  
+      process.stdout.write('your node version is ' + process.versions.node+'\n');
+      break;
+
+    case '/lang':
+      process.stdout.write('your system language is ' + process.env.LANG+'\n'); 
+      break;
+
+    default:
+      process.stderr.write('wrong instruction\n');
+    }
 }});
