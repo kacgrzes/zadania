c2d4d217494fcec0fc1b8a0f7def347a9ec962de
szopizzle <maciejdabek@gmail.com>
2017-02-03 11:30:03 +0100

    poprawione

--- npmtest/modules/timeFormat.js
+++ npmtest/modules/timeFormat.js
@@ -1,19 +1,15 @@
-var os=require("os");
-var colors=require('colors');
+var os = require('os');
+var colors = require('colors');
 function timeFormat(){
-	
-	var uptime=os.uptime();
-	var hours=Math.floor(uptime/3600);
-    var minutes=Math.floor((uptime-hours*3600)/60);
-    var seconds=Math.floor(uptime-(hours*3600+minutes*60));
-	if(uptime>=3600){
-		console.log("uptime:".cyan+hours+"h"+minutes+"min"+seconds+"s");
+  var uptime = os.uptime();
+  var hours = Math.floor(uptime / 3600);
+  var minutes = Math.floor((uptime - hours * 3600) / 60);
+  var seconds = Math.floor(uptime - (hours * 3600+minutes * 60));
+    if(uptime >= 3600){
+	    console.log('uptime:'.cyan+hours+'h'+minutes+'min'+seconds+'s');
+    } else{
+	    console.log('uptime:'.cyan+hours+'h'+minutes+'min'+seconds+'s');
 	}
-	
-	else{
-		console.log("uptime:".cyan+hours+"h"+minutes+"min"+seconds+"s");
-	}
-	
 }
 
-exports.print=timeFormat;
+exports.print = timeFormat;
