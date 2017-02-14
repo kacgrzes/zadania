94f35f0177094de9c497d9830a5f770f8d421641
maciek <maciejdabek@gmail.com>
2017-02-14 15:14:53 +0100

    zadanie react 2_6

---
+++ 2_6/script.js
@@ -0,0 +1,86 @@
+var Counter = React.createClass({
+	
+	getInitialState: function(){
+		return{
+			counter : 0
+			};
+
+		console.log('getInitialState');
+	},
+
+	increment: function(){
+		this.setState({
+			counter: this.state.counter + 1
+		});
+	},
+
+	decrement: function(){
+		this.setState({
+			counter: this.state.counter - 1
+		});
+	},
+
+	render: function(){
+		return React.createElement('div', {},
+			React.createElement('button', {onClick: this.decrement}, '-'),
+			React.createElement('span', {}, ' Licznik ' + this.state.counter + ' '),
+			React.createElement('button', {onClick: this.increment}, '+')
+			
+			);
+	},
+
+	getDefaultProps: function() {
+        console.log('Pobiera propsy')
+    },
+
+	 
+	componentWillMount(){
+		console.log('componentWillMount  - zaraz nastapi załadowanie komponentu')
+   },
+   
+   componentDidMount(){
+   		console.log('componentDidMount, załadowano komponent, tu można coś wgrac z servera, manipulowac DOMem')
+   },
+
+   componentWillReceiveProps(newProps){
+   		console.log('componentWillReceiveProps - sprawdza nowe propsy')
+   },
+
+   shouldComponentUpdate(newProps, newState){
+   		console.log('shouldComponentUpdate - sprawdza czy re-renderować');
+   		return true;
+   },
+
+   componentWillUpdate(nextProps, nextState){
+   		console.log('componentWillUpdate - bo zmiana w stanie, więc można re-renderować' );
+   },
+
+   componentDidUpdate(prevProps, prevState){
+   		console.log('componentDidUpdate - komponent zaktualizowany')
+   },
+
+   componentWillUnmount(){
+   		console.log('componentWillUnmount komponent zostal zdemontowany z DOM-u')
+   },
+
+})
+
+var Unmount = React.createClass({
+	demontuj : function (){
+		console.log('bla');
+		ReactDOM.unmountComponentAtNode(document.getElementById('app'));
+	},
+
+	render: function(){
+
+		return  React.createElement('button',{onClick: this.demontuj}, 'unmount')
+
+	}
+
+})
+
+
+
+var element = React.createElement('div', {},
+	React.createElement(Counter, {}),React.createElement(Counter,{}), React.createElement(Unmount, {}));
+ReactDOM.render(element, document.getElementById('app'));
